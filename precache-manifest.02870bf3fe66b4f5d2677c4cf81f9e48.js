8e732d6af2349dad44c4e7bfe879b59d8a76e6df
StefanLjiljak <stefanljiljak@gmail.com>
2021-01-20 13:15:57 +0100

    Updates

---
+++ precache-manifest.02870bf3fe66b4f5d2677c4cf81f9e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964fd0fd003fe360f94b7a0e82289754",
+    "url": "/deposit-box/index.html"
+  },
+  {
+    "revision": "1231a127e510e666d9a3",
+    "url": "/deposit-box/static/css/main.cbc6e4c5.chunk.css"
+  },
+  {
+    "revision": "4d95a7c0bd3f929a0dfe",
+    "url": "/deposit-box/static/js/2.643ca842.chunk.js"
+  },
+  {
+    "revision": "1231a127e510e666d9a3",
+    "url": "/deposit-box/static/js/main.1975f4d0.chunk.js"
+  },
+  {
+    "revision": "e4146bd47591717c50bd",
+    "url": "/deposit-box/static/js/runtime~main.b9062fa1.js"
+  }
+]);
